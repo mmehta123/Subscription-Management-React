ae87b9bb5ba40c4e6024c051a99ca314d033148d
mmehta123 <mohitmehta285@gmail.com>
2022-05-07 15:37:19 +0530

    9.Toggle Form in NewSubscription

--- src/components/NewSubscription/NewSubscription.js
+++ src/components/NewSubscription/NewSubscription.js
@@ -1,10 +1,16 @@
+import { useState } from "react";
 import FormSubscription from "./FormSubscription";
 import "./NewSubscription.css"
 
 const NewSubscription=(props)=>{
+    const [showForm,setShowForm]=useState(true);
+    const showFormBtnHandler=()=>{
+        setShowForm(!showForm)
+    }
     return(
         <div className="new_subscription">
-            <FormSubscription formToNew={props.newSubToApp} />
+            {showForm && <FormSubscription formToNew={props.newSubToApp} />}
+            <button onClick={showFormBtnHandler}>Toggle Form</button>
         </div>
     );
 }
