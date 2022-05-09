07f38baf5dc9009bb9c6d71b710f53a0969c3487
mmehta123 <mohitmehta285@gmail.com>
2022-05-09 15:43:41 +0530

    3.useEffect with local storage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
-import  Container  from "./templates/Container";
+import Container from "./templates/Container";
 import NewSubscription from './components/NewSubscription/NewSubscription';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Filter from './components/NewSubscription/Filter';
 import SubscriptionList from './components/NewSubscription/SubscriptionList';
 import SubscriptionChart from './components/NewSubscription/SubscriptionChart';
@@ -27,34 +27,39 @@ let initialSubscriptions = [
   }
 ]
 function App() {
-  const [subscriptions,setSubscription]=useState(initialSubscriptions);
-  const[filteredYear,setFilteredYear]=useState("2020");
-  if(localStorage.getItem('filteredYear')){
-    //BUG:::::: got local storage item but this if statement sets the state again and again with same value and starts an infinte loop
-    setFilteredYear(localStorage.getItem('filteredyear'))
+  const [subscriptions, setSubscription] = useState(initialSubscriptions);
+  const [filteredYear, setFilteredYear] = useState("2020");
+  // so if there is any previous filtered year saved then it will first load usestate with data with localstorage year
+  // if nothing is saved it will not run show by default 2020 filteredYear data
+  useEffect(() => {
+    if (localStorage.getItem('filteredYear')) {
+      setFilteredYear(localStorage.getItem('filteredYear'))
+      console.log("run useEffect" + localStorage.getItem('filteredYear'))
+    }
+  }, [])
+
+  const newSubToApp = (data) => {
+    setSubscription([data, ...subscriptions])
   }
 
-  const newSubToApp=(data)=>{
-    setSubscription([data, ...subscriptions] ) 
+  const dataFromFilter = (year) => {
+    setFilteredYear(year);
+    // it saves filtered year in local storage of browser see in inspect application option
+    localStorage.setItem('filteredYear', year)
+    console.log("run datafromfilter", localStorage.getItem('filteredYear', year))
   }
 
-  const dataFromFilter=(year)=>{
-        setFilteredYear(year);
-        // saved filtered year in local storage of browser see in inspect application option
-        localStorage.setItem('filteredYear',year)
-  }
-
-  const filteredArr=subscriptions.filter((item)=>{
-    return item.date.getFullYear().toString()===filteredYear;
+  const filteredArr = subscriptions.filter((item) => {
+    return item.date.getFullYear().toString() === filteredYear;
   })
-  
+
   return (
     <Container>
-      <NewSubscription newSubToApp={newSubToApp}/>
-      <Filter filteredData={filteredYear} dataFromFilter={dataFromFilter}/>
+      <NewSubscription newSubToApp={newSubToApp} />
+      <Filter filteredData={filteredYear} dataFromFilter={dataFromFilter} />
       {/* LOOK Here */}
-      <SubscriptionChart filterSubscription={filteredArr}/>
-      <SubscriptionList list={filteredArr}/>
+      <SubscriptionChart filterSubscription={filteredArr} />
+      <SubscriptionList list={filteredArr} />
     </Container>
   );
 }
